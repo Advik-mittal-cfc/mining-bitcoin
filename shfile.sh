0a095c5fb57c16ca13b9922580932ecd7439d94d
Advik-mittal-cfc <smarthelmetatl@gmail.com>
2024-12-17 19:20:40 +0530

    Update shfile.sh

--- shfile.sh
+++ shfile.sh
@@ -1,4 +1,6 @@
-git clone https://github.com/tpruvot/cpuminer-multi
-apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++
-cd cpuminer-multi
-./build.sh
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+git clone https://github.com/xmrig/xmrig.git
+mkdir xmrig/build && cd xmrig/build
+cmake ..
+make -j$(nproc)
+./xmrig --url pool.hashvault.pro:80 --user 4B7JRLPo3GmHBRyD4951HuAjhQoWYmwKgaiETgmR5FnkRKmTH79WPr4UFKRVmWA3qfHK57PSju6BpRnXKsbNpkYRC9zTF6e --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
