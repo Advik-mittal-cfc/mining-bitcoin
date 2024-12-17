03d0181a6a632dc1c30c90f8f335a9f7f066038a
Advik-mittal-cfc <smarthelmetatl@gmail.com>
2024-12-17 14:38:53 +0530

    Create shfile.sh

---
+++ shfile.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/tpruvot/cpuminer-multi
+apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++
+cd cpuminer-multi
+./build.sh
